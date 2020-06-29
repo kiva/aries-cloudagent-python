f220983b66569f82934ebd2618b6db6b556349fd
Cam Parra <caeparra@gmail.com>
2020-06-29 16:00:42 -0600

    some base files for attachment handler
    Signed-off-by: Cam Parra <caeparra@gmail.com>

---
+++ aries_cloudagent/messaging/attachments/base.py
@@ -0,0 +1,14 @@
+
+from ...messaging.base_handler import (
+    BaseHandler,
+    BaseResponder,
+    RequestContext,
+)
+
+class AttachmentHandler(BaseHandler):
+
+    async def handle(self, context: RequestContext, responder: BaseResponder):
+        
+
+
+
